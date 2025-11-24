eceadc529639fab6792041d55ad595c8c9f4ec95
hppanpaliya <hppanpaliya@gmail.com>
2025-11-24 18:39:45 -0500

    update

--- assets/chunk-4BX2VUAB-Al9-lfRk.js
+++
@@ -1,1 +0,0 @@
-import{_ as i}from"./ProjectModal-PA94F4Cq.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
